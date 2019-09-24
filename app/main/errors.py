a7631eb9730242c03575c53bf8bc9d53bf6bc064
rithaven <veneruth4@gmail.com>
2019-09-24 12:56:06 +0200

    Add comments form

---
+++ app/main/errors.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from . import main
+
+@main.app_errorhandler(404)
+def four_Ow_four.html(error):
+    '''
+    Function to render the 404 error page
+    '''
+
+    return render_template('fourOwfour.html'),404
+    
