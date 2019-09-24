6cf4baa04db66d2706af5ec50374e35dee590b1c
rithaven <veneruth4@gmail.com>
2019-09-24 18:30:50 +0200

    Add index file

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/wecode/Desktop/Pictures/virtual"
+set -gx VIRTUAL_ENV "/home/wecode/Desktop/Pitches-IT/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
